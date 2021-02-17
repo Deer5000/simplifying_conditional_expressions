8f8ab6daa6c01ea3cd27f66d580fe3a5c7a59601
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-16 20:27:59 -0500

    finished exercise 13

--- lab/refactoring/consolidate_conditional.py
+++ lab/refactoring/consolidate_conditional.py
@@ -1,5 +1,6 @@
 # by Kami Bigdely
 # Consolidate conditional expressions
+all_ingredients = ['cucumber','tomato','onion','lemon juice']
 def dice(ingredients):
     print("diced all ingredients.")
 def mix_all(diced_ingredients):
@@ -10,23 +11,14 @@ def pour(liquid):
     print('poured', liquid + '.',)
 
 def make_shirazi_salad(ingredients):
-    if 'cucumber' not in ingredients:
+    if 'cucumber' or 'tomato' or 'onion' or 'lemon juice' not in ingredients:
         print('lacks ingredients.')
-        return
-    if 'tomato' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'onion' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'lemon juice' not in ingredients:
-        print('lacks ingredients.')
-        return
-    dice(ingredients)
-    mix_all(ingredients)
-    add_salt()
-    pour('lemon juice')
-    print('Your yummy shirazi salad is ready!')
-
+        dice(ingredients)
+        mix_all(ingredients)
+        add_salt()
+        pour('lemon juice')
+        print('Your yummy shirazi salad is ready!')
+    else:
+        print("You have all the ingredients for your salad and it is ready!")
 if __name__ == "__main__":
     make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
