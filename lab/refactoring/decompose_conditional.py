562d84701ec40de6e5172d5c2fb37be6c7fbf316
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-18 13:17:52 -0500

    exercise 18 complete

--- lab/refactoring/decompose_conditional.py
+++ lab/refactoring/decompose_conditional.py
@@ -7,15 +7,47 @@ def make_alert_sound():
 def make_accept_sound():
     print('made acceptance sound')
 
-ingredients = ['sodium benzoate']
-if 'sodium nitrate' in ingredients or 'sodium benzoate' in ingredients\
-or 'sodium oxide' in ingredients:
-    print('!!!')
-    print('there is a toxin in the food!')    
-    print('!!!')
-    make_alert_sound()
-else:
-    print('***')
-    print('Toxin Free')
-    print('***')
-    make_accept_sound()
+toxins = ['sodium nitrate', 'sodium benzonate', 'sodium oxide']
+# toxin1 = 'sodium nitrate'
+# toxin2 = 'sodium benzonate'
+# toxin3 = 'sodium oxide'
+
+ingredients = 'sodium benzonate'
+def toxin_check(food):
+    for i in toxins:
+        if i == ingredients:
+            print('!!!')
+            print('there is a toxin in the food!')    
+            print('!!!')
+            make_alert_sound()
+            break
+        else:
+            print('***')
+            print('Toxin Free')
+            print('***')
+            make_accept_sound()
+
+toxin_check(ingredients)
+# if toxin1 or toxin2 or toxin3 == ingredients:
+#     print('!!!')
+#     print('there is a toxin in the food!')    
+#     print('!!!')
+#     make_alert_sound()
+# else:
+#     print('***')
+#     print('Toxin Free')
+#     print('***')
+#     make_accept_sound()
+
+
+# for i in toxins:
+#     if i == ingredients:
+#         print('!!!')
+#         print('there is a toxin in the food!')    
+#         print('!!!')
+#         make_alert_sound()
+#     else:
+#         print('***')
+#         print('Toxin Free')
+#         print('***')
+#         make_accept_sound()
